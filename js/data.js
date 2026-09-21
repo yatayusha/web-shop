4e5ad6a45b864dd5e8f045b49c8dbb5a4657bc3f
Taissiya Skorintseva <taissiyaskorintseva@MacBook-Air-Taissiya.local>
2026-09-21 18:52:14 +0300

    images were created

--- js/data.js
+++ js/data.js
@@ -1,5 +1,11 @@
 const products = [
-    { id: 1, name: 'Товар 1', price: 1000, image: 'https://via.placeholder.com/150' },
-    { id: 2, name: 'Товар 2', price: 1500, image: 'https://via.placeholder.com/150' },
-    { id: 3, name: 'Товар 3', price: 800, image: 'https://via.placeholder.com/150' },
+    { id: 1, name: '', price: 3200, image: 'images/dress1.jpg' },
+    { id: 2, name: '', price: 4500, image: 'images/dress2.jpg' },
+    { id: 3, name: '', price: 3900, image: 'images/dress3.jpg' },
+    { id: 4, name: '', price: 3600, image: 'images/dress4.jpg' },
+    { id: 5, name: '', price: 6800, image: 'images/dress5.jpg' },
+    { id: 6, name: '', price: 2900, image: 'images/dress6.jpg' },
+    { id: 7, name: '', price: 4100, image: 'images/dress7.jpg' },
+    { id: 8, name: '', price: 3300, image: 'images/dress8.jpg' },
+    { id: 9, name: '', price: 5200, image: 'images/dress9.jpg' },
   ];
