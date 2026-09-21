7d7bea005eb09521163d13dcb78b28f48d15a361
Taissiya Skorintseva <taissiyaskorintseva@MacBook-Air-Taissiya.local>
2026-09-21 19:34:38 +0300

    style improvements

--- js/data.js
+++ js/data.js
@@ -1,11 +1,8 @@
 const products = [
-    { id: 1, name: '', price: 3200, image: 'images/dress1.jpg' },
-    { id: 2, name: '', price: 4500, image: 'images/dress2.jpg' },
-    { id: 3, name: '', price: 3900, image: 'images/dress3.jpg' },
-    { id: 4, name: '', price: 3600, image: 'images/dress4.jpg' },
-    { id: 5, name: '', price: 6800, image: 'images/dress5.jpg' },
-    { id: 6, name: '', price: 2900, image: 'images/dress6.jpg' },
-    { id: 7, name: '', price: 4100, image: 'images/dress7.jpg' },
-    { id: 8, name: '', price: 3300, image: 'images/dress8.jpg' },
-    { id: 9, name: '', price: 5200, image: 'images/dress9.jpg' },
+    { id: 1, name: 'Платье Jess', price: 3200, image: 'images/Jess.webp' },
+    { id: 2, name: 'Платье Heidi', price: 4500, image: 'images/Heidi.webp' },
+    { id: 3, name: 'Платье Charm', price: 3900, image: 'images/Charm.webp' },
+    { id: 4, name: 'Платье Jules', price: 3600, image: 'images/Jules.webp' },
+    { id: 5, name: 'Платье Rose', price: 6800, image: 'images/Rose.webp' },
+    { id: 6, name: 'Платье Vannessa', price: 2900, image: 'images/Vanessa.webp' },
   ];
